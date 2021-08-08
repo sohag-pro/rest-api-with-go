215a1d023716ac6d656dfeba8dd03dd734a7f45f
Sohag Hasan <sohag.web.dev@gmail.com>
2021-08-08 19:29:11 +0600

    sqlite3 DB connection added

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.16.0
+	github.com/jinzhu/gorm v1.9.16
 )
--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"restapi/database"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+
+
+func initDatabase(){
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+
+	if err != nil {
+		panic("Failed to connect to DB")
+	}
+
+	fmt.Println("DB connection Successfull")
+}
 
 func main(){
 	// Fiber Instance
 	app := fiber.New()
+
+	// Init DB
+	initDatabase()
+	defer database.DBConn.Close()
 
 	// Route
 	handleRoute(app)
